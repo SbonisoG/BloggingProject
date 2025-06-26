58b39527751ac9a8fffac187ade84212c570dab3
Sboniso <sboniso.sbo49@gmail.com>
2025-06-26 18:20:23 +0200

    Add project files.

---
+++ BloggingProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BloggingProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
