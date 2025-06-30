32f8bba40459968c7c86aadccf589171cbbbb292
Sboniso <sboniso.sbo49@gmail.com>
2025-06-30 15:05:47 +0200

    Add control, added view, added bootswatch(bootstrap theme)

---
+++ BloggingProject/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using BloggingProject.Data;
+using BloggingProject.Models.ViewsModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BloggingProject.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly BlogDbContext _context;
+        public PostController(BlogDbContext context) 
+        {
+            _context = context;
+        }
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Create Post";
+
+            var postViewModel = new PostViewModel();
+            postViewModel.Categories = _context.Categories.Select(c =>
+            new SelectListItem
+            {
+                Value = c.CategoryId.ToString(),
+                Text = c.Name
+            }
+            ).ToList();
+
+            return View(postViewModel);
+        }
+    }
+}
---
+++ BloggingProject/Models/ViewsModel/PostViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BloggingProject.Models.ViewsModel
+{
+    public class PostViewModel
+    {
+        public Post Post { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IFormFile FeatureImage { get; set; }
+    }
+}
